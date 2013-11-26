02ae372780371509543d9c4844fb806292ddddda
Blackvein <alexc.1995@hotmail.com>
2013-11-26 10:11:21 -0800

    Made TalkingMob class
    Fixed up FrostByten's code

--- src/me/blackvein/mobtalk/MobTalk.java
+++ src/me/blackvein/mobtalk/MobTalk.java
@@ -7,24 +7,99 @@ package me.blackvein.mobtalk;
  * Version: 0.1.0b
  ******************/
 
-import org.bukkit.command.ConsoleCommandSender;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
 import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.EntityType;
+import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class MobTalk extends JavaPlugin
 {
+    private int RADIUS;
+    private final HashSet<TalkingMob> talkingMobs = new HashSet<>();
+    
     @Override
-    public void onEnable() //Called when the plugin is enabled
+    public void onEnable()
     {
-        ConsoleCommandSender console = getServer().getConsoleSender();
-        getServer().getPluginManager().registerEvents(new EventListener(this), this); //Register the event listener
+        getServer().getPluginManager().registerEvents(new EventListener(this), this);
         saveDefaultConfig();
         FileConfiguration config = getConfig();
+        RADIUS = config.getInt("maximumDistance");
     }
     
     @Override
-    public void onDisable() //Called when the plugin is disabled
+    public void onDisable()
     {
-        super.onDisable(); //Default shutdown behaviour
+        super.onDisable();
     }
+    
+    /**
+     * <p>Searches for nearby players to hear mob speech. Uses the RADIUS
+     * constant as distance in all directions.</p>
+     * 
+     * @param ent The mob to search from
+     * @return List of found players, or null if none found
+     */
+    public List<Player> getPlayersNear(Entity ent)
+    {
+        List<Entity> list = ent.getNearbyEntities(RADIUS, RADIUS, RADIUS);
+        List<Player> out = new ArrayList<>();
+        for(Entity e : list)
+        {
+            if(e instanceof Player)
+            {
+                Player player = (Player) e;
+                out.add(player);
+            }
+        }
+        
+        return(!out.isEmpty() ? out : null);
+    }
+    
+    /**
+     * <p>Returns appropriate MobTalk message from config.</p>
+     * 
+     * @param ent The mob entity
+     * @param type Message type
+     * @return Message found, or null if not found
+     */
+    public String getChatString(Entity ent, String type)
+    {
+       return null; 
+    }
+    
+    /**
+     * <p>Retrieves a TalkingMob to acquire messages from.</p>
+     * 
+     * @param et Mob EntityType
+     * @param child Whether the type is a child or not
+     * @return TalkingMob found, or null if not found
+     */
+    public TalkingMob getTalkingMob(EntityType et, boolean child){
+        
+        for(TalkingMob t : talkingMobs){
+            
+            if(t.getMobType().equals(et) && t.isChild() == child)
+                return t;
+            
+        }
+        
+        return null;
+        
+    }
+    
+    public void LEWIS_READ_ME(){
+        
+        //THIS IS HOW YOU DO THE TALKINGMOB CLASS
+        TalkingMob tm = new TalkingMob(EntityType.COW, false); //false cause not child
+        tm.addRandomMessage("lol im a cow");
+        tm.addAttackedMessage("OWWWWWWW YOU HIT MEEEE");
+        talkingMobs.add(tm);
+        
+    }
+    
+    
 }
---
+++ src/me/blackvein/mobtalk/TalkingMob.java
@@ -0,0 +1,268 @@
+package me.blackvein.mobtalk;
+
+import java.util.ArrayList;
+import java.util.Random;
+import org.bukkit.entity.EntityType;
+
+public class TalkingMob
+{
+    
+    private static final Random random = new Random();
+    
+    private final EntityType mob;
+    private final boolean child;
+    
+    private final ArrayList<String> randomMessages = new ArrayList<>();
+    private final ArrayList<String> spawnMessages = new ArrayList<>();
+    private final ArrayList<String> targetMessages = new ArrayList<>();
+    private final ArrayList<String> attackMessages = new ArrayList<>();
+    private final ArrayList<String> attackedMessages = new ArrayList<>();
+    private final ArrayList<String> deathMessages = new ArrayList<>();
+    private final ArrayList<String> interactMessages = new ArrayList<>();
+    
+    private final ArrayList<String> bornMessages = new ArrayList<>();
+    private final ArrayList<String> breedMessages = new ArrayList<>();
+    
+    public TalkingMob(EntityType mob, boolean child)
+    {
+        this.mob = mob;
+        this.child = child;
+    }
+    
+    /**
+     * <p>Returns the MobType of the TalkingMob.</p>
+     * 
+     * @return the MobType
+     */
+    public EntityType getMobType()
+    {
+        return mob;
+    }
+    
+    /**
+     * <p>Returns whether the TalkingMob is a child or not.</p>
+     * 
+     * @return true if child, false if not.
+     */
+    public boolean isChild()
+    {
+        return child;
+    }
+    
+    /**
+     * <p>Returns a random "random" message. This message is to be used
+     * when a mob is idling.</p>
+     * 
+     * @return Random message, or null if none are available.
+     */
+    public String getRandomMessage()
+    {
+        if(randomMessages.isEmpty())
+            return null;
+        else
+            return randomMessages.get(random.nextInt(randomMessages.size()));
+    }
+    
+    /**
+     * <p>Adds a "random" message to the TalkingMob.</p>
+     * 
+     * @param message The message to add
+     */
+    public void addRandomMessage(String message)
+    {
+        randomMessages.add(message);
+    }
+    
+    /**
+     * <p>Returns a random "spawn" message. This message is to be used
+     * when a mob spawns.</p>
+     * 
+     * @return Random message, or null if none are available.
+     */
+    public String getSpawnMessage()
+    {
+        if(spawnMessages.isEmpty())
+            return null;
+        else
+            return spawnMessages.get(random.nextInt(spawnMessages.size()));
+    }
+    
+    /**
+     * <p>Adds a "spawn" message to the TalkingMob.</p>
+     * 
+     * @param message The message to add
+     */
+    public void addSpawnMessage(String message)
+    {
+        spawnMessages.add(message);
+    }
+    
+    /**
+     * <p>Returns a random "target" message. This message is to be used
+     * when a mob gains a new target.</p>
+     * 
+     * @return Random message, or null if none are available.
+     */
+    public String getTargetMessage()
+    {
+        if(targetMessages.isEmpty())
+            return null;
+        else
+            return targetMessages.get(random.nextInt(targetMessages.size()));
+    }
+    
+    /**
+     * <p>Adds a "target" message to the TalkingMob.</p>
+     * 
+     * @param message The message to add
+     */
+    public void addTargetMessage(String message)
+    {
+        targetMessages.add(message);
+    }
+    
+    /**
+     * <p>Returns a random "attack" message. This message is to be used
+     * when a mob attacks a target.</p>
+     * 
+     * @return Random message, or null if none are available.
+     */
+    public String getAttackMessage()
+    {
+        if(attackMessages.isEmpty())
+            return null;
+        else
+            return attackMessages.get(random.nextInt(attackMessages.size()));
+    }
+    
+    /**
+     * <p>Adds an "attack" message to the TalkingMob.</p>
+     * 
+     * @param message The message to add
+     */
+    public void addAttackMessage(String message)
+    {
+        attackMessages.add(message);
+    }
+    
+    /**
+     * <p>Returns a random "attacked" message. This message is to be used
+     * when a mob is attacked.</p>
+     * 
+     * @return Random message, or null if none are available.
+     */
+    public String getAttackedMessage()
+    {
+        if(attackedMessages.isEmpty())
+            return null;
+        else
+            return attackedMessages.get(random.nextInt(attackedMessages.size()));
+    }
+    
+    /**
+     * <p>Adds an "attacked" message to the TalkingMob.</p>
+     * 
+     * @param message The message to add
+     */
+    public void addAttackedMessage(String message)
+    {
+        attackedMessages.add(message);
+    }
+    
+    /**
+     * <p>Returns a random "death" message. This message is to be used
+     * when a mob dies.</p>
+     * 
+     * @return Random message, or null if none are available.
+     */
+    public String getDeathMessage()
+    {
+        if(deathMessages.isEmpty())
+            return null;
+        else
+            return deathMessages.get(random.nextInt(deathMessages.size()));
+    }
+    
+    /**
+     * <p>Adds a "death" message to the TalkingMob.</p>
+     * 
+     * @param message The message to add
+     */
+    public void addDeathMessage(String message)
+    {
+        deathMessages.add(message);
+    }
+    
+    /**
+     * <p>Returns a random "interact" message. This message is to be used
+     * when a mob is interacted with (right-clicked on).</p>
+     * 
+     * @return Random message, or null if none are available.
+     */
+    public String getInteractMessage()
+    {
+        if(interactMessages.isEmpty())
+            return null;
+        else
+            return interactMessages.get(random.nextInt(interactMessages.size()));
+    }
+    
+    /**
+     * <p>Adds an "interact" message to the TalkingMob.</p>
+     * 
+     * @param message The message to add
+     */
+    public void addInteractMessage(String message)
+    {
+        interactMessages.add(message);
+    }
+    
+    /**
+     * <p>Returns a random "born" message. This message is to be used
+     * when a baby mob is born.</p>
+     * 
+     * @return Random message, or null if none are available.
+     */
+    public String getBornMessage()
+    {
+        if(bornMessages.isEmpty())
+            return null;
+        else
+            return bornMessages.get(random.nextInt(bornMessages.size()));
+    }
+    
+    /**
+     * <p>Adds a "born" message to the TalkingMob.</p>
+     * 
+     * @param message The message to add
+     */
+    public void addBornMessage(String message)
+    {
+        bornMessages.add(message);
+    }
+    
+    /**
+     * <p>Returns a random "random" message. This message is to be used
+     * when a mob breeds with another.</p>
+     * 
+     * @return Random message, or null if none are available.
+     */
+    public String getBreedMessage()
+    {
+        if(breedMessages.isEmpty())
+            return null;
+        else
+            return breedMessages.get(random.nextInt(breedMessages.size()));
+    }
+    
+    /**
+     * <p>Adds a "breed" message to the TalkingMob.</p>
+     * 
+     * @param message The message to add
+     */
+    public void addBreedMessage(String message)
+    {
+        breedMessages.add(message);
+    }
+    
+}
